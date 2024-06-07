70cf314eefa9c470e74d9d0a1c15fe6028157cd7
darienjonathan <darien.jonathan@w.ntt.com>
2024-06-07 17:24:33 +0900

    add docs template at Home

--- react/src/components/pages/Home/index.tsx
+++ react/src/components/pages/Home/index.tsx
@@ -1,1 +1,54 @@
-export const Home: React.FC = () => <>Home</>;
+import styles from "./index.module.css";
+
+const header = ["機能", "React", "Vue", "メモ"];
+
+const contents = [
+  {
+    title: "Counter",
+    content: [
+      ["ステート管理", "useState", "ref", "値が変わると再レンダリングが走る"],
+      [
+        "ステートから計算した値",
+        "何も使わない、それともuseMemo",
+        "computed",
+        "vueではsetupは一回しか走らないため、computedでラップする必要がある",
+      ],
+    ],
+  },
+  {
+    title: "Todo",
+    content: [
+      ["ステート管理", "useState", "ref", "値が変わると再レンダリングが走る"],
+      [
+        "ステートから計算した値",
+        "何も使わない、それともuseMemo",
+        "computed",
+        "vueではsetupは一回しか走らないため、computedでラップする必要がある",
+      ],
+    ],
+  },
+];
+
+export const Home: React.FC = () => (
+  <div className={styles.table_group}>
+    {contents.map((content) => (
+      <div className={styles.table}>
+        <div className={styles.table_title}>{content.title}</div>
+        <div className={styles.table_grid}>
+          {[header, ...content.content].map((content, index) => (
+            <>
+              {content.map((item) => (
+                <div
+                  key={index}
+                  className={index === 0 ? styles.table_header : ""}
+                >
+                  {item}
+                </div>
+              ))}
+            </>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+);
